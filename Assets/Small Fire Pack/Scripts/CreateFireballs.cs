f4640131e674d78ad434415c46dce00d3651a2f5
bl00d <k_osh@inbox.ru>
2025-05-12 18:56:25 +0300

    update props + add fire assets

---
+++ Assets/Small Fire Pack/Scripts/CreateFireballs.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateFireballs : MonoBehaviour {
+
+    public Rigidbody Fireball;
+
+    private bool CreateInstances = true;
+    private Rigidbody Instance;
+    public float Time = 1.0f;
+
+	void Start () {
+        Create();
+        InvokeRepeating("Create", 1.0f, Time);
+	}
+    void Update()
+    {
+        if(Instance == null)
+        {
+            CreateInstances = true;
+        }
+
+    }
+
+    void Create()
+    {
+        if (CreateInstances)
+        {
+            Instance = Instantiate(Fireball, transform.position, transform.rotation);
+            Instance.transform.SetParent(transform.parent);
+            CreateInstances = false;
+        }
+        
+    }
+}
